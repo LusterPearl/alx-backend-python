107f1a2b02761aaefa80b1bb6295b6a215602aeb
LusterPearl <Salamioluwatosin90@gmail.com>
2024-04-22 14:03:06 +0100

    ttask3

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -10,6 +10,21 @@ from typing import Callable
 from 1-concurrent_coroutines import wait_n
 
 
+async def wait_random(max_delay: int) -> float:
+    """waiting period for random"""
+    delay = random.unifrom(0, max_delay)
+    await asyncio.sleep(delay)
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """ waiting time for max delay"""
+    start_time = time.time()
+    await asyncio.gather(*delays)
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time
+
+
 def measure_time(n: int, max_delay: int) -> float:
     """
      Measure the total execution time of wait_n(n, max_delay)
@@ -22,8 +37,4 @@ def measure_time(n: int, max_delay: int) -> float:
     Returns:
         float: The average execution time per iteration.
         """
-    start_time = time.time()
-    asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
-    total_time = end_time - start_time
-    return total_time / n
+    return asyncio.run(wait_n(n, max_delay)) / n

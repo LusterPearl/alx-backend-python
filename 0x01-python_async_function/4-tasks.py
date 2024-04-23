0162309c66d0ac1d8eaa262fafe12d6262ee59b2
LusterPearl <Salamioluwatosin90@gmail.com>
2024-04-23 11:23:43 +0000

    done

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,6 +9,7 @@ from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Returns a list of delays sorted in ascending order.

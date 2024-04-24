3eb5176df7d357369d943dbc12a267cbb72d92b7
LusterPearl <Salamioluwatosin90@gmail.com>
2024-04-24 12:11:35 +0100

    task0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 """
-Async Generator Module
+Async Generator Module that yields random numbers
 """
 
 import asyncio
 import random
 
+from typing import Generator
 
-async def async_generator():
+
+async def async_generator() -> Generator[float, None, None]:
     """
     Async generator that yields random numbers between 0 and 10
     """

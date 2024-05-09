f6d6f992bd3bda68561d8f20164cbd084825f795
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-09 13:27:59 +0100

    readjustedtask1

--- 0x03-Unittests_and_integration_tests/tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/tests/test_utils.py
@@ -26,4 +26,4 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
 
-        self.assertEqual(str(context.exception), f"KeyError('{key_error_msg}')")
+        self.assertEqual(context.exception.args[0], key_error_msg)

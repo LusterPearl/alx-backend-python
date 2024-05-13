5875a223a9d87a4e8f4026a9b688894674c67281
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-13 13:30:40 +0100

    parmclass

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,6 +6,7 @@ import unittest
 from unittest.mock import patch
 from client import GithubOrgClient
 from parameterized import parameterized_class
+from parameterized import parameterized
 from fixtures import TEST_PAYLOAD
 
 class TestGithubOrgClient(unittest.TestCase):

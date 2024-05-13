543ac9b28934406f7062024ab9666cf0e42bde3b
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-13 13:26:55 +0100

    ne1

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,7 +6,7 @@ import unittest
 from unittest.mock import patch
 from client import GithubOrgClient
 from parameterized import parameterized
-from fixtures import TEST_PAYLOAD, org_payload, repos_payload, expected_repos, apache2_repos
+from fixtures import TEST_PAYLOAD, repos_payload, expected_repos, apache2_repos
 
 class TestGithubOrgClient(unittest.TestCase):
     """Test case for GithubOrgClient class."""
